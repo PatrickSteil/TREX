78e5b08c7676efe3c84991a2d036b4023a9db61b
PatrickSteil <patricksteil@freenet.de>
2026-03-10 16:46:52 +0100

    add changes to timestamped reachedindex

--- Algorithms/TripBased/TimestampedReachedIndex.h
+++ Algorithms/TripBased/TimestampedReachedIndex.h
@@ -27,70 +27,109 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #include <algorithm>
 
-#include "../../../DataStructures/TripBased/Data.h"
+#include "../../DataStructures/TripBased/Data.h"
+
+static constexpr uint32_t LABEL_MASK = 0xFF;
+static constexpr uint32_t DEFAULT_MASK = 0xFF00;
+static constexpr uint32_t TS_SHIFT = 16;
 
 namespace TripBased {
 
 class TimestampedReachedIndex {
- public:
-  TimestampedReachedIndex(const Data& data)
-      : data(data),
-        labels(data.numberOfTrips(), -1),
-        timestamps(data.numberOfTrips(), 0),
-        timestamp(0),
-        defaultLabels(data.numberOfTrips(), -1) {
-    for (const TripId trip : data.trips()) {
-      if (data.numberOfStopsInTrip(trip) > 255)
-        warning("Trip ", trip, " has ", data.numberOfStopsInTrip(trip),
-                " stops!");
-      defaultLabels[trip] = data.numberOfStopsInTrip(trip);
+public:
+  TimestampedReachedIndex(const Data &data)
+      : data(data), state(data.numberOfTrips()), routeEnd(data.numberOfTrips()),
+        timestamp(1) {
+
+    for (TripId trip : data.trips()) {
+      uint8_t stops = data.numberOfStopsInTrip(trip);
+
+      if (stops > 255)
+        warning("Trip ", trip, " has ", stops, " stops!");
+
+      uint32_t packed =
+          (uint32_t(timestamp) << 16) | (uint32_t(stops) << 8) | stops;
+
+      state[trip] = packed;
+
+      routeEnd[trip] = data.firstTripOfRoute[data.routeOfTrip[trip] + 1];
     }
   }
 
- public:
+public:
   inline void clear() noexcept {
     ++timestamp;
+
     if (timestamp == 0) {
-      labels = defaultLabels;
-      std::fill(timestamps.begin(), timestamps.end(), 0);
+      uint32_t *__restrict ptr = state.data();
+      const size_t n = state.size();
+
+      for (size_t i = 0; i < n; ++i) {
+        uint32_t s = ptr[i];
+        uint8_t def = (s & DEFAULT_MASK) >> 8;
+        ptr[i] = (uint32_t(def) << 8) | def;
+      }
+
+      timestamp = 1;
     }
   }
 
   inline StopIndex operator()(const TripId trip) noexcept {
-    AssertMsg(trip < labels.size(), "Trip " << trip << " is out of bounds!");
+    AssertMsg(trip < state.size(), "Trip " << trip << " is out of bounds!");
     return StopIndex(getLabel(trip));
   }
 
-  inline bool alreadyReached(const TripId trip, const u_int8_t index) noexcept {
-    return getLabel(trip) <= index;
+  inline bool alreadyReached(TripId trip, uint8_t index) noexcept {
+    uint32_t s = state[trip];
+
+    uint8_t label = ((s >> TS_SHIFT) == timestamp) ? (s & LABEL_MASK)
+                                                   : ((s >> 8) & LABEL_MASK);
+
+    return label <= index;
   }
 
-  inline void update(const TripId trip, const StopIndex index) noexcept {
-    AssertMsg(trip < labels.size(), "Trip " << trip << " is out of bounds!");
-    const TripId routeEnd = data.firstTripOfRoute[data.routeOfTrip[trip] + 1];
-    for (TripId i = trip; i < routeEnd; i++) {
-      u_int8_t& label = getLabel(i);
-      if (label <= index) break;
-      label = index;
+  inline void update(TripId trip, StopIndex index) noexcept {
+    uint32_t *__restrict statePtr = state.data();
+    const uint32_t end = routeEnd[trip];
+    const uint8_t newValue = index;
+
+    for (uint32_t i = trip; i < end; ++i) {
+      uint32_t s = statePtr[i];
+
+      uint16_t ts = s >> TS_SHIFT;
+      uint8_t label =
+          (ts == timestamp) ? (s & LABEL_MASK) : ((s >> 8) & LABEL_MASK);
+
+      if (label <= newValue)
+        break;
+
+      uint8_t def = (s >> 8) & LABEL_MASK;
+
+      statePtr[i] =
+          (uint32_t(timestamp) << TS_SHIFT) | (uint32_t(def) << 8) | newValue;
     }
   }
 
- private:
-  inline u_int8_t& getLabel(const TripId trip) noexcept {
+private:
+  inline uint8_t &getLabel(const TripId trip) noexcept {
     if (timestamps[trip] != timestamp) {
-      labels[trip] = defaultLabels[trip];
+      state[trip] = defaultLabels[trip];
       timestamps[trip] = timestamp;
     }
-    return labels[trip];
+    return state[trip];
   }
 
-  const Data& data;
+  const Data &data;
 
-  std::vector<u_int8_t> labels;
-  std::vector<u_int16_t> timestamps;
-  u_int16_t timestamp;
-
-  std::vector<u_int8_t> defaultLabels;
+  // [ timestamp (16) | default (8) | label (8) ]
+  /*
+  bits 0..7   = current label
+  bits 8..15  = default label
+  bits 16..31 = timestamp
+  */
+  std::vector<uint32_t> state;
+  std::vector<uint32_t> routeEnd;
+  uint16_t timestamp;
 };
 
-}  // namespace TripBased
+} // namespace TripBased
